10faac8571935ed13adca40be2cb5bbf2441655d
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 02:51:17 +0300

    feat: add particular common type id3v2 frames

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/Models/Frames/ID3V2PictureFrame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ID3V2_ClassLibrary.Models.Frames
+{
+    class ID3V2PictureFrame : ID3V2Frame
+    {
+        public ID3V2PictureFrame(byte[] rawFrame) : base(rawFrame)
+        {
+        }
+    }
+}
---
+++ ID3V2-Editor/ID3V2-ClassLibrary/Models/Frames/ID3V2TextFrame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ID3V2_ClassLibrary.Models.Frames
+{
+    class ID3V2TextFrame : ID3V2Frame
+    {
+        public ID3V2TextFrame(byte[] rawFrame) : base(rawFrame)
+        {
+        }
+
+        public string GetContent()
+        {
+            byte[] rawContent = rawFrame.Skip(10).ToArray();
+
+            return Encoding.Unicode.GetString(rawContent);
+        }
+    }
+}
