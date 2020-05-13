f40977b1f0b7ee8e5389eb45daf16ae9a2c1815f
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 01:25:36 +0300

    feat: add ReadHeader method to ID3V2Editor class

--- ID3V2-Editor/ID3V2-ClassLibrary/ID3V2Editor.cs
+++ ID3V2-Editor/ID3V2-ClassLibrary/ID3V2Editor.cs
@@ -9,10 +9,20 @@ namespace ID3V2_ClassLibrary
     {
         private Stream fileStream;
         private ID3V2Header header;
-        
+
         public ID3V2Editor(IFileManager fileManager)
         {
             fileStream = fileManager.Open();
+            ReadHeader();
+        }
+
+        private void ReadHeader()
+        {
+            byte[] rawHeader = new byte[10];
+            fileStream.Read(rawHeader, 0, 10);
+
+            header = new ID3V2Header(rawHeader);
+            if (!header.isValid) throw new InvalidID3V2Exception();
         }
     }
 }
