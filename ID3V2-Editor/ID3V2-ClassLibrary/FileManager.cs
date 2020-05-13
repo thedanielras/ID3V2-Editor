efd14f77e322d77f9e86be5ed7027c4c8ece56fb
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 00:58:02 +0300

    feat: add ID3V2Editor class

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ID3V2_ClassLibrary
+{
+    class FileManager : IFileManager
+    {
+        private string path;
+
+        public FileManager(string path)
+        {
+            this.path = path;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (File.Exists(path))
+                throw new InvalidFileException(path);
+        }
+
+        public Stream Open()
+        {
+            return new FileStream(path, FileMode.Open);
+        }
+    }
+}
