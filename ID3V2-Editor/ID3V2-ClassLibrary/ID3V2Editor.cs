ca7628cc8c366f6ca581b5d43bc87bba394af5c0
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 01:03:03 +0300

    feat: add ID3V2Editor class

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/ID3V2Editor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ID3V2_ClassLibrary
+{
+    class ID3V2Editor
+    {
+        private Stream fileStream;
+        private ID3V2Header header;
+        
+        public ID3V2Editor(IFileManager fileManager)
+        {
+            fileStream = fileManager.Open();
+        }
+    }
+}
