b499f7a6e338fd2b8adc354cc9eb42f72873626c
Daniel Ras <ras98.daniel@gmail.com>
2020-05-13 23:48:50 +0300

    feat: add Application class

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ID3V2_ClassLibrary
+{
+    class Application
+    {
+        public Application(string path)
+        {
+
+        }
+    }
+}
