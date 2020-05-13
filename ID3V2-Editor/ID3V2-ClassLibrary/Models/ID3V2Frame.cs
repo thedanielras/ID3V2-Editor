93bbb59e016ae3f7161fa18c1b76c6c3fc7019d5
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 01:54:48 +0300

    feat: add ID3V2Frame model class

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/Models/ID3V2Frame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ID3V2_ClassLibrary.Models
+{
+    abstract class ID3V2Frame
+    {
+        protected byte[] rawFrame;
+
+        public ID3V2Frame(byte[] rawFrame)
+        {
+            this.rawFrame = rawFrame;
+        }
+
+        public string ID { get; private set; }
+        public int Size { get; private set; }
+    }
+}
