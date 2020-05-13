21ad13e6d39d8025f3984b5a05e2dd894814c0d9
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 01:21:31 +0300

    feat: add InvalidID3V2Exception exception

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/Exceptions/InvalidID3V2Exception.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ID3V2_ClassLibrary
+{
+    [Serializable]
+    internal class InvalidID3V2Exception : Exception
+    {     
+        public InvalidID3V2Exception() : base("ID3V2 corrupted or not present!")
+        {
+        }
+    }
+}
