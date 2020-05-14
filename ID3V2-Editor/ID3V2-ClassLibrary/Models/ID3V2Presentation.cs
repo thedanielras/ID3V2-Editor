d5e1a742f69a09db6482f21de8964f164351ee96
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 03:23:08 +0300

    feat: add ID3V2Presentation class

---
+++ ID3V2-Editor/ID3V2-ClassLibrary/Models/ID3V2Presentation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ID3V2_ClassLibrary.Models
+{
+    class ID3V2Presentation
+    {
+        public ID3V2Presentation()
+        {
+
+        }
+
+        public string Artist { get; private set; }
+        public string Album { get; private set; }
+    }
+}
