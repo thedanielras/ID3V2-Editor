e341d8b1c7dbcbc87e0164f1ed0ac4cbaaa4fe4d
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 03:23:40 +0300

    feat: update Application class to use ID3V2Editor

--- ID3V2-Editor/ID3V2-ClassLibrary/Application.cs
+++ ID3V2-Editor/ID3V2-ClassLibrary/Application.cs
@@ -6,9 +6,17 @@ namespace ID3V2_ClassLibrary
 {
     public class Application
     {
+        private ID3V2Editor editor;
+
         public Application(string path)
         {
+            IFileManager fileManager = new FileManager(path);
+            editor = new ID3V2Editor(fileManager);
+        }
 
+        public ID3V2Editor GetID3V2()
+        {
+            return editor;
         }
     }
 }
