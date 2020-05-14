b0d1eb35f80744717342e9ff401ece51f4e18406
Daniel Ras <ras98.daniel@gmail.com>
2020-05-14 14:12:07 +0300

    feat: update ID3V2TextFrame to recognize encoding

--- ID3V2-Editor/ID3V2-ClassLibrary/Models/Frames/ID3V2TextFrame.cs
+++ ID3V2-Editor/ID3V2-ClassLibrary/Models/Frames/ID3V2TextFrame.cs
@@ -14,8 +14,28 @@ namespace ID3V2_ClassLibrary.Models.Frames
         public string GetContent()
         {
             byte[] rawContent = rawFrame.Skip(10).ToArray();
+            int encodingFlag = Convert.ToInt32(rawContent[0]);
 
-            return Encoding.BigEndianUnicode.GetString(rawContent);
+            Encoding encoder = Encoding.Default;
+            bool hasPreamble = false;
+
+            if (encodingFlag == 0) encoder = Encoding.ASCII;
+            else if (encodingFlag == 1)
+            {
+                hasPreamble = true;
+                encoder = (rawContent[1] == 0xFE) ? Encoding.BigEndianUnicode : Encoding.Unicode;
+            }
+
+            // Skip encoding byte
+            rawContent = rawContent.Skip(1).ToArray();
+            
+            if (hasPreamble)
+            {
+                //skip preamble
+                rawContent = rawContent.Skip(2).ToArray();
+            }
+
+            return encoder.GetString(rawContent).Replace("\0", String.Empty);
         }
     }
 }
